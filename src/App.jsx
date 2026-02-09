f5bc1c898e3457eaeb47a144154df041d0b853a6
Kacper Czernecki <walentyczer@gmail.com>
2026-02-09 03:01:56 +0100

    Added basic logic with visualCrossing API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {}
+function App() {
+  const [cityInput, setCityInput] = useState("");
+  const [city, setCity] = useState("Warszawa");
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  useEffect(() => {
+    if (!apiKey) {
+      setLoading(false);
+      setError(new Error("Missing API key"));
+      return;
+    }
+    const fetchData = async () => {
+      setData(null);
+      setError(null);
+      setLoading(true);
+      try {
+        const res = await axios.get(
+          `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(city)}?unitGroup=metric&key=${apiKey}`,
+        );
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [city, apiKey]);
+
+  const handleClick = () => {
+    if (!cityInput.trim()) return;
+    setCity(cityInput);
+  };
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data) return null;
+  return (
+    <div className="container">
+      <input
+        type="text"
+        value={cityInput}
+        placeholder="Enter a City"
+        onChange={(e) => setCityInput(e.target.value)}
+      />
+      <button onClick={handleClick}>Search</button>
+      <h2>Weather in {city}</h2>
+      <p>Temperature: {data.currentConditions.temp}°C</p>
+      <p>Humidity: {data.currentConditions.humidity}%</p>
+    </div>
+  );
+}
 
 export default App;
